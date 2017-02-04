7b498e7f4a3916524a90aff759d4f2659a09759e
wanghui <wanghui-it@bestpay.com.cn>
2017-02-04 09:43:47 +0800

    first commit

---
+++ src/main/java/cn/com/bestpay/quartz/quartzDemoJob.java
@@ -0,0 +1,16 @@
+package cn.com.bestpay.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by user on 2017/1/13.
+ */
+public class quartzDemoJob implements Job{
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("执行job……");
+    }
+}
